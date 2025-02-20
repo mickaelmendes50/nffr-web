60daf5aa9e01d29297430f919ccb6cffec28d4f8
Mesquita <mickaelmendes50@gmail.com>
2025-02-19 21:09:22 -0300

    app: Initial theme inicialization
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Greeting from '@/components/Greeting.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+    <img alt="NFFR logo" class="logo" src="./assets/nffr-logo.png" width="130" height="130" />
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Greeting />
     </div>
   </header>
-
   <main>
-    <TheWelcome />
   </main>
 </template>
 
--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <h1>Leitura Arquetípica</h1>
+    <h3>Responda as questões com atenção para obter o resultado assertivo.</h3>
   </div>
 </template>
 
